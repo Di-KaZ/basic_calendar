8f0297cc707ea69863de6e5f9c3098dbd527ca9d
GET_MOUSSED <m.fofana@come-con.co>
2023-10-10 11:27:42 +0200

    initial commit

---
+++ example/calendar_basic_example.dart
@@ -0,0 +1,6 @@
+import 'package:calendar_basic/calendar_basic.dart';
+
+void main() {
+  var awesome = Awesome();
+  print('awesome: ${awesome.isAwesome}');
+}
---
+++ test/calendar_basic_test.dart
@@ -0,0 +1,16 @@
+import 'package:calendar_basic/calendar_basic.dart';
+import 'package:test/test.dart';
+
+void main() {
+  group('A group of tests', () {
+    final awesome = Awesome();
+
+    setUp(() {
+      // Additional setup goes here.
+    });
+
+    test('First Test', () {
+      expect(awesome.isAwesome, isTrue);
+    });
+  });
+}
